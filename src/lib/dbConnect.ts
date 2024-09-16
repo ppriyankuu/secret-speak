644e038b53f47cd23ae857502905304536ad2a76
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-09-16 19:25:08 +0530

    db-connect function

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,14 +7,12 @@ type ConnectionObject = {
 const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
-  // Check if we have a connection to the database or if it's currently connecting
   if (connection.isConnected) {
     console.log('Already connected to the database');
     return;
   }
 
   try {
-    // Attempt to connect to the database
     const db = await mongoose.connect(process.env.DB_URL ?? '', {});
 
     connection.isConnected = db.connections[0].readyState;
@@ -23,7 +21,6 @@ async function dbConnect(): Promise<void> {
   } catch (error) {
     console.error('Database connection failed:', error);
 
-    // Graceful exit in case of a connection error
     process.exit(1);
   }
 }
